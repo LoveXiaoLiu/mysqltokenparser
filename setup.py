7c8ea44aff84c26307ee953c79884c8dec78ee11
caoshuai03 <caoshuai03@58.com>
2019-11-14 18:23:34 +0800

    change Development to 2

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     author="gra55",
     author_email='shuai.grass@gmail.com',
     classifiers=[
-        'Development Status ::  5 - Production/Stable',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
