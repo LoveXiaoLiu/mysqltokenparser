1aa13ee35412bcc7fdb199bbcbf5e8fff003975b
caoshuai03 <caoshuai03@58.com>
2019-11-15 14:32:37 +0800

    Bump version: 2.1.1 → 2.1.2

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/LoveXiaoLiu/mysqltokenparser',
-    version='2.1.1',
+    version='2.1.2',
     zip_safe=False,
 )
